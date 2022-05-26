c46405d3a1342f0ec93f2f21acfd1932197d432b
hoact99 <hoact.it@gmail.com>
2022-05-26 23:41:04 +0700

    Fix some bugs and add lib: Summernote

--- app/views/includes/style.php
+++ app/views/includes/style.php
@@ -1,12 +1,12 @@
 <!-- Bootstrap Styles-->
-<link href="<?= HTTP_ROOT; ?>/public/assets/css/bootstrap.css" rel="stylesheet" />
+<link href="<?= HTTP_ROOT; ?>public/assets/css/bootstrap.css" rel="stylesheet" />
 <!-- FontAwesome Styles-->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" />
 <!-- Morris Chart Styles-->
-<link href="<?= HTTP_ROOT; ?>/public/assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+<link href="<?= HTTP_ROOT; ?>public/assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
 <!-- Custom Styles-->
-<link href="<?= HTTP_ROOT; ?>/public/assets/css/custom-styles.css" rel="stylesheet" />
+<link href="<?= HTTP_ROOT; ?>public/assets/css/custom-styles.css" rel="stylesheet" />
 <!-- Google Fonts-->
 <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
 <!-- SummerNote CSS -->
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<link href="<?= HTTP_ROOT; ?>public/assets/css/summernote.min.css" rel="stylesheet">
